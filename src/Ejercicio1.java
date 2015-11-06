90a80ab8c415cff9aa0bc96146e8000cf81146f4
Jaime Martínez <jaime.martinez.ortega@gmail.com>
2015-11-06 17:19:20 +0100

    Inicio del Proyecto
    Mensaje en la pantalla

---
+++ src/Ejercicio1.java
@@ -0,0 +1,15 @@
+/*
+ * Autor: Jaime Martínez
+ * Ejemplo de uso de GitHub
+ */
+
+
+import acm.graphics.*;
+import acm.program.GraphicsProgram;
+
+public class Ejercicio1 extends GraphicsProgram{
+	public void run (){
+		GLabel etiqueta = new GLabel ("HOLA GIT");
+		add (etiqueta, 120,130);
+	}	
+}
