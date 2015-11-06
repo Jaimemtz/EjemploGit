fc39c8b216aaf12790604c8edf18bf0f8f0d0c5b
Jaime Martínez <jaime.martinez.ortega@gmail.com>
2015-11-06 19:23:18 +0100

    Finalizada piramide invertida
    fata todavia girar

--- src/Ejercicio1.java
+++ src/Ejercicio1.java
@@ -15,6 +15,16 @@ public class Ejercicio1 extends GraphicsProgram{
 		pintaPiramide();
 	}	
 	public void pintaPiramide(){
+		int numLadrillosBase = 14;
+		int anchoLadrillo = 30;
+		int altoLadrillo = 12;
+		//querido yo del martes te falta girar la piramide 
+		for (int j=0;  j< numLadrillosBase; j++){
+			for (int i=0; i< numLadrillosBase-j; i++){
+				GRect ladrillo = new GRect (anchoLadrillo, altoLadrillo);
+				add(ladrillo, anchoLadrillo * j/2 + anchoLadrillo * i, altoLadrillo*j); 
 
+			}
+		}
 	}
 }
